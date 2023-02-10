65ceb7d1caf0cb454e07bd93f3c086c9695b0272
Tycodra <tyco.draper@gmail.com>
2023-02-09 17:05:09 -0700

    Starting de-duplicating background tasks.

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/GetFollowingTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/GetFollowingTask.java
@@ -2,8 +2,6 @@ package edu.byu.cs.tweeter.client.model.service.backgroundTask;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 
 import java.io.Serializable;
 import java.util.List;
@@ -16,14 +14,11 @@ import edu.byu.cs.tweeter.util.Pair;
 /**
  * Background task that retrieves a page of other users being followed by a specified user.
  */
-public class GetFollowingTask implements Runnable {
+public class GetFollowingTask extends BackgroundTask {
     private static final String LOG_TAG = "GetFollowingTask";
-
-    public static final String SUCCESS_KEY = "success";
-    public static final String FOLLOWEES_KEY = "followees";
     public static final String MORE_PAGES_KEY = "more-pages";
-    public static final String MESSAGE_KEY = "message";
-    public static final String EXCEPTION_KEY = "exception";
+    public static final String FOLLOWEES_KEY = "followees";
+
 
     /**
      * Auth token for logged-in user.
@@ -43,77 +38,32 @@ public class GetFollowingTask implements Runnable {
      * This allows the new page to begin where the previous page ended.
      */
     private User lastFollowee;
-    /**
-     * Message handler that will receive task results.
-     */
-    private Handler messageHandler;
+
+    List<User> followees ;
+    boolean hasMorePages;
 
     public GetFollowingTask(AuthToken authToken, User targetUser, int limit, User lastFollowee,
                             Handler messageHandler) {
+        super(messageHandler);
         this.authToken = authToken;
         this.targetUser = targetUser;
         this.limit = limit;
         this.lastFollowee = lastFollowee;
-        this.messageHandler = messageHandler;
     }
-
-    @Override
-    public void run() {
-        try {
-            Pair<List<User>, Boolean> pageOfUsers = getFollowees();
-
-            List<User> followees = pageOfUsers.getFirst();
-            boolean hasMorePages = pageOfUsers.getSecond();
-
-            sendSuccessMessage(followees, hasMorePages);
-
-        } catch (Exception ex) {
-            Log.e(LOG_TAG, "Failed to get followees", ex);
-            sendExceptionMessage(ex);
-        }
-    }
-
-    private FakeData getFakeData() {
-        return FakeData.getInstance();
-    }
-
     private Pair<List<User>, Boolean> getFollowees() {
         return getFakeData().getPageOfUsers((User) lastFollowee, limit, targetUser);
     }
 
+    @Override
+    protected void runTask() {
+        Pair<List<User>, Boolean> pageOfUsers = getFollowees();
+        followees = pageOfUsers.getFirst();
+        hasMorePages = pageOfUsers.getSecond();
+    }
 
-    private void sendSuccessMessage(List<User> followees, boolean hasMorePages) {
-        Bundle msgBundle = new Bundle();
-        msgBundle.putBoolean(SUCCESS_KEY, true);
+    @Override
+    protected void loadMessage(Bundle msgBundle) {
         msgBundle.putSerializable(FOLLOWEES_KEY, (Serializable) followees);
         msgBundle.putBoolean(MORE_PAGES_KEY, hasMorePages);
-
-        Message msg = Message.obtain();
-        msg.setData(msgBundle);
-
-        messageHandler.sendMessage(msg);
     }
-
-    private void sendFailedMessage(String message) {
-        Bundle msgBundle = new Bundle();
-        msgBundle.putBoolean(SUCCESS_KEY, false);
-        msgBundle.putString(MESSAGE_KEY, message);
-
-        Message msg = Message.obtain();
-        msg.setData(msgBundle);
-
-        messageHandler.sendMessage(msg);
-    }
-
-    private void sendExceptionMessage(Exception exception) {
-        Bundle msgBundle = new Bundle();
-        msgBundle.putBoolean(SUCCESS_KEY, false);
-        msgBundle.putSerializable(EXCEPTION_KEY, exception);
-
-        Message msg = Message.obtain();
-        msg.setData(msgBundle);
-
-        messageHandler.sendMessage(msg);
-    }
-
 }
